85a72795dd41a22b7e0543cb2c715de672f06c70
stdmatt <stdmatt@pixelwizards.io>
2020-03-10 11:43:01 +0300

    Make the project to build with the new libs again

--- src/Simple_Tree.js
+++ src/Simple_Tree.js
@@ -30,6 +30,7 @@ const ANGLE_MAX       = 50;
 
 const SPEED_TO_GROW = 150;
 
+
 //----------------------------------------------------------------------------//
 // Variables                                                                  //
 //----------------------------------------------------------------------------//
@@ -42,6 +43,15 @@ let curr_speed = 0;
 //----------------------------------------------------------------------------//
 // Helper Functions                                                           //
 //----------------------------------------------------------------------------//
+function CreateVector(x, y)
+{
+    return {x:x, y:y};
+}
+function hslToRgb(h, s, l)
+{
+    return [255, 255, 255, 255]
+}
+
 class Branch
 {
     constructor(x, y, size, angle, distanceToRoot)
@@ -51,8 +61,8 @@ class Branch
 
         this.distanceToRoot = distanceToRoot;
 
-        this.start  = Math_CreateVector(x, y);
-        this.end    = Math_CreateVector(
+        this.start  = CreateVector(x, y);
+        this.end    = CreateVector(
             x + (size * Math.cos(angle * Math.PI / 180)),
             y + (size * Math.sin(angle * Math.PI / 180))
         );
@@ -100,7 +110,7 @@ function DrawBranch(b, distanceSoFar)
     let x2 = x1 + (len * l) * Math.cos(ang * Math.PI / 180);
     let y2 = y1 + (len * l) * Math.sin(ang * Math.PI / 180);
 
-    let h = Math_Map(distanceSoFar, 0, tree[tree.length-1].distanceToRoot, 0, 360);
+    let h   = Math_Map(distanceSoFar, 0, tree[tree.length-1].distanceToRoot, 0, 360);
     let rgb = hslToRgb(h / 360, 0.5, 0.5);
     let s = "rgb(" + rgb[0] + ","
                    + rgb[1] + ","
@@ -117,10 +127,10 @@ function DrawBranch(b, distanceSoFar)
 
 function GenerateTree()
 {
-    let generations = Math_RandomInt(GENERATIONS_MIN, GENERATIONS_MAX);
-    let size        = Math_Random(SIZE_MIN,        SIZE_MAX);
-    let decay       = Math_Random(DECAY_MIN,       DECAY_MAX);
-    let angle       = Math_Random(ANGLE_MIN,       ANGLE_MAX);
+    let generations = Random_Int   (GENERATIONS_MIN, GENERATIONS_MAX);
+    let size        = Random_Number(SIZE_MIN,        SIZE_MAX);
+    let decay       = Random_Number(DECAY_MIN,       DECAY_MAX);
+    let angle       = Random_Number(ANGLE_MIN,       ANGLE_MAX);
 
     tree       = [];
     total_len  = 0;
@@ -128,8 +138,8 @@ function GenerateTree()
     curr_speed = SPEED_TO_GROW;
 
     CreateTree(generations, size, decay, angle);
-    Log(generations, tree.length);
 }
+
 
 //----------------------------------------------------------------------------//
 // Setup / Draw                                                               //
@@ -137,7 +147,34 @@ function GenerateTree()
 //------------------------------------------------------------------------------
 function Setup()
 {
+    Random_Seed(null);
+
+    //
+    // Configure the Canvas.
+    const parent        = document.getElementById("canvas_div");
+    const parent_width  = parent.clientWidth;
+    const parent_height = parent.clientHeight;
+
+    const max_side = Math_Max(parent_width, parent_height);
+    const min_side = Math_Min(parent_width, parent_height);
+
+    const ratio = min_side / max_side;
+
+    // Landscape
+    if(parent_width > parent_height) {
+        Canvas_CreateCanvas(800, 800 * ratio, parent);
+    }
+    // Portrait
+    else {
+        Canvas_CreateCanvas(800 * ratio, 800, parent);
+    }
+
+    Canvas.style.width  = "100%";
+    Canvas.style.height = "100%";
+
+
     GenerateTree();
+    Canvas_Draw(0);
 }
 
 
@@ -171,10 +208,4 @@ function Draw(dt)
 //----------------------------------------------------------------------------//
 // Entry Point                                                                //
 //----------------------------------------------------------------------------//
-Canvas_Setup({
-    main_title        : "Simple Trees",
-    main_date         : "Jul 19, 2019",
-    main_version      : "v0.0.1",
-    main_instructions : "<br>Move your mouse closer to the edge to increase speed",
-    main_link: "<a href=\"http://stdmatt.com/demos/startfield.html\">More info</a>"
-});
+Setup();
