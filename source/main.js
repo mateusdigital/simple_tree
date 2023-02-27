e4ec2dfe0e1497444112ebf7b7d2f1d678bd8299
mateus-earth <mateus@cosmicpig.digital>
2023-02-27 14:50:41 -0300

    Tween is broken...

--- source/main.js
+++ source/main.js
@@ -10,15 +10,25 @@
 //  Date      : Aug 25, 2019                                                  //
 //  License   : GPLv3                                                         //
 //  Author    : stdmatt <stdmatt@pixelwizards.io>                             //
-//  Copyright : stdmatt 2019, 2020                                            //
+//  Copyright : stdmatt 2019, 2020, 2023                                      //
 //                                                                            //
 //  Description :                                                             //
 //                                                                            //
 //---------------------------------------------------------------------------~//
 
+
 //----------------------------------------------------------------------------//
 // Constants                                                                  //
 //----------------------------------------------------------------------------//
+//------------------------------------------------------------------------------
+__SOURCES = [
+    "/modules/demolib/modules/external/chroma.js",
+    "/modules/demolib/modules/external/gif.js/gif.js",
+
+    "/modules/demolib/source/demolib.js",
+];
+
+//------------------------------------------------------------------------------
 const GENERATIONS_MIN = 5;
 const GENERATIONS_MAX = 7;
 const SIZE_MIN        = 100;
@@ -34,22 +44,22 @@ const ANIM_GROW_DURATION_MAX = 3500;
 const MIN_TREES_COUNT = 3;
 const MAX_TREES_COUNT = 7;
 
-
 //----------------------------------------------------------------------------//
 // Variables                                                                  //
 //----------------------------------------------------------------------------//
-var background_color = chroma.rgb(221, 227, 213).name();
-var tree_color       = chroma.rgb(102,  80,  93).name();
+var background_color = "black";
+var tree_color       = "blue";
 var trees            = [];
 
 
 //----------------------------------------------------------------------------//
 // Helper Functions                                                           //
 //----------------------------------------------------------------------------//
+//------------------------------------------------------------------------------
 function CreateTree()
 {
-    const tree_root_space = (Canvas_Half_Width * 0.8);
-    const tree_root_x = Random_Int(-tree_root_space, +tree_root_space);
+    const tree_root_space = (get_canvas_width() * 0.5 * 0.8);
+    const tree_root_x = random_int(-tree_root_space, +tree_root_space);
     trees.push(new Tree(tree_root_x));
 }
 
@@ -76,30 +86,30 @@ class Branch
         this.curr_angle = currentAngle;
         this.curr_size  = currentSize;
 
-        this.start = Vector_Create(x, y);
-        this.end   = Vector_Create(
-            x + (this.curr_size * Math_Cos(Math_Radians(this.curr_angle))),
-            y + (this.curr_size * Math_Sin(Math_Radians(this.curr_angle)))
+        this.start = make_vec2(x, y);
+        this.end   = make_vec2(
+            x + (this.curr_size * Math.cos(to_radians(this.curr_angle))),
+            y + (this.curr_size * Math.sin(to_radians(this.curr_angle)))
         );
 
         // Generation.
         this.curr_generation = currentGeneration;
 
         // Animation.
-        this.anim_grow_duration = Random_Int(
+        this.anim_grow_duration = random_int(
             ANIM_GROW_DURATION_MIN,
             ANIM_GROW_DURATION_MAX
         );
-        this.anim_grow_tween = Tween_CreateBasic(
+        this.anim_grow_tween = Tween.create(
             this.anim_grow_duration,
             this.parent_tree.anim_grow_group
         )
-        .onComplete(()=>{
+        .on_complete(()=>{
             this._CreateSubBranch();
         });
 
         if(this.curr_generation == 0) {
-            this.anim_grow_tween.delay(Random_Int(100, 4000))
+            this.anim_grow_tween.delay(random_int(100, 4000))
         }
         this.anim_grow_tween.start();
 
@@ -110,18 +120,15 @@ class Branch
     //--------------------------------------------------------------------------
     Draw(dt)
     {
-        const  t = this.anim_grow_tween.getValue().value;
+        const  t = this.anim_grow_tween.get_value().value;
         const x1 = this.start.x;
         const y1 = this.start.y;
 
-        const x2 = Math_Lerp(x1, this.end.x, t);
-        const y2 = Math_Lerp(y1, this.end.y, t);
+        const x2 = lerp(x1, this.end.x, t);
+        const y2 = lerp(y1, this.end.y, t);
 
-        // @notice(stdmatt): спасибо моей хорошей жене that at very early in the
-        // morning just said how I should make the branches thicker near to the
-        // root ;D
-        Canvas_SetStrokeSize((this.parent_tree.max_generations / (this.curr_generation + 1)))
-        Canvas_DrawLine(x1, y1, x2, y2);
+        set_canvas_line_width(this.parent_tree.max_generations / (this.curr_generation + 1));
+        draw_line(x1, y1, x2, y2);
 
         for(let i = 0; i < this.branches.length; ++i) {
             this.branches[i].Draw(dt);
@@ -138,23 +145,23 @@ class Branch
         const new_generation = this.curr_generation + 1;
         // @improve(stdmatt): This way we make the branches to grow bigger
         // slightly to the left... Can be improved...
-        const t1 = Random_Number(0.6, 1);
-        const t2 = Random_Number(t1, 1);
+        const t1 = random_float(0.6, 1);
+        const t2 = random_float(t1, 1);
 
         const left_branch = new Branch(
-            Math_Lerp(this.start.x, this.end.x, t1),
-            Math_Lerp(this.start.y, this.end.y, t1),
-            this.curr_size  * Random_Number(DECAY_MIN, DECAY_MAX),
-            this.curr_angle - Random_Number(ANGLE_MIN, ANGLE_MAX),
+            lerp(this.start.x, this.end.x, t1),
+            lerp(this.start.y, this.end.y, t1),
+            this.curr_size  * random_float(DECAY_MIN, DECAY_MAX),
+            this.curr_angle - random_float(ANGLE_MIN, ANGLE_MAX),
             new_generation,
             this.parent_tree
         );
 
         const right_branch = new Branch(
-            Math_Lerp(this.start.x, this.end.x, t2),
-            Math_Lerp(this.start.y, this.end.y, t2),
-            this.curr_size  * Random_Number(DECAY_MIN, DECAY_MAX),
-            this.curr_angle + Random_Number(ANGLE_MIN, ANGLE_MAX),
+            lerp(this.start.x, this.end.x, t2),
+            lerp(this.start.y, this.end.y, t2),
+            this.curr_size  * random_float(DECAY_MIN, DECAY_MAX),
+            this.curr_angle + random_float(ANGLE_MIN, ANGLE_MAX),
             new_generation,
             this.parent_tree
         );
@@ -171,31 +178,31 @@ class Tree
     //--------------------------------------------------------------------------
     constructor(x)
     {
-        this.max_generations = Random_Int(GENERATIONS_MIN, GENERATIONS_MAX);
+        this.max_generations = random_int(GENERATIONS_MIN, GENERATIONS_MAX);
         this.color           = chroma(tree_color);
         this.is_done         = false;
 
         // Animations.
-        this.anim_grow_group = Tween_CreateGroup()
-            .onComplete(()=>{
+        this.anim_grow_group = Tween.create_with_group()
+            .on_complete(()=>{
                 this.anim_die_tween.start();
             });
 
         // @todo(stdmatt): Remove magic numbers...
-        this.anim_die_tween = Tween_CreateBasic(Random_Int(1000, 3000))
-            .delay(Random_Int(500, 2500))
-            .onUpdate((v)=>{
+        this.anim_die_tween = Tween.create(random_int(1000, 3000))
+            .delay(random_int(500, 2500))
+            .on_update((v)=>{
                 this.color = this.color.alpha(1 - v.value)
             })
-            .onComplete(()=>{
+            .on_complete(()=>{
                 this.is_done = true;
             })
 
         this.branch = new Branch(
             x,
-            Canvas_Edge_Bottom,
-            Random_Int(SIZE_MIN,  SIZE_MAX),
-            -90 +  Random_Number(-10, +10),
+            get_canvas_height(),
+            random_int(SIZE_MIN,  SIZE_MAX),
+            -90 +  random_float(-10, +10),
             0, // current generation
             this
         );
@@ -204,7 +211,7 @@ class Tree
     //--------------------------------------------------------------------------
     Draw(dt)
     {
-        Canvas_SetStrokeStyle(this.color);
+        set_canvas_stroke(this.color)
         this.anim_grow_group.update();
         this.branch.Draw(dt);
     } // Draw
@@ -213,7 +220,6 @@ class Tree
     StartToDie()
     {
         this.is_dying = true;
-
     } // StartToDie
 }; // class Tree
 
@@ -221,77 +227,82 @@ class Tree
 // Setup / Draw                                                               //
 //----------------------------------------------------------------------------//
 //------------------------------------------------------------------------------
-function Setup()
+function setup_standalone_mode()
 {
-    const seed = 1;
-    Random_Seed(seed);
+    return new Promise((resolve, reject)=>{
+        demolib_load_all_scripts(__SOURCES).then(()=> { // Download all needed scripts.
+            // Create the standalone canvas.
+            const canvas = document.createElement("canvas");
 
-    //
-    // Configure the Canvas.
-    const parent        = document.getElementById("canvas_div");
-    const parent_width  = parent.clientWidth;
-    const parent_height = parent.clientHeight;
+            canvas.width            = window.innerWidth;
+            canvas.height           = window.innerHeight;
+            canvas.style.position   = "fixed";
+            canvas.style.left       = "0px";
+            canvas.style.top        = "0px";
+            canvas.style.zIndex     = "-100";
 
-    const max_side = Math_Max(parent_width, parent_height);
-    const min_side = Math_Min(parent_width, parent_height);
-    const ratio    = (min_side / max_side);
+            document.body.appendChild(canvas);
 
-    // Landscape
-    if(parent_width > parent_height) {
-        Canvas_CreateCanvas(800, 800 * ratio, parent);
-    }
-    // Portrait
-    else {
-        Canvas_CreateCanvas(800 * ratio, 800, parent);
-    }
+            // Setup the listener for gif recording.
+            gif_setup_listeners();
 
-    Canvas.style.width  = "100%";
-    Canvas.style.height = "100%";
+            resolve(canvas);
+        });
+    });
+}
 
-    //
-    // Add the information.
-    const info = document.createElement("p");
-    info.innerHTML = String_Cat(
-        "Simple Tree", "<br>",
-        "Aug 25, 2019", "<br>",
-        GetVersion(),   "<br>",
-        "<a href=\"http://stdmatt.com/demos/simple_tree.html\">More info</a>"
-    );
-    parent.appendChild(info);
+//------------------------------------------------------------------------------
+function setup_common(canvas)
+{
+    set_random_seed();
+
+    set_main_canvas(canvas);
+    set_canvas_fill("white");
 
     //
     // Create the Trees.
-    const trees_count = Random_Int(MIN_TREES_COUNT, MAX_TREES_COUNT);
+    const trees_count = random_int(MIN_TREES_COUNT, MAX_TREES_COUNT);
     for(let i = 0; i < trees_count; ++i) {
         CreateTree();
     }
 
-    //
-    // Start the Simulation.
-    Canvas_Start();
+    translate_canvas_to_center();
+    start_draw_loop(draw);
 }
+
 
 
 //------------------------------------------------------------------------------
-function Draw(dt)
+function demo_main(user_canvas)
 {
-    Canvas_ClearWindow(background_color);
-
-    Tween_Update(dt);
-    for(let i = trees.length -1; i >= 0; --i) {
-        const tree = trees[i];
-        if(tree.is_done) {
-            Array_RemoveAt(trees, i);
-            CreateTree();
-
-            continue;
-        }
-        tree.Draw(dt);
+    if(!user_canvas) {
+        setup_standalone_mode().then((canvas)=>{
+            setup_common(canvas);
+        });
+    } else {
+        canvas = user_canvas;
+        setup_common();
     }
+
 }
 
+//------------------------------------------------------------------------------
+function draw(dt)
+{
+    clear_canvas();
 
-//----------------------------------------------------------------------------//
-// Entry Point                                                                //
-//----------------------------------------------------------------------------//
-Setup();
+    begin_draw();
+        Tween_Group._default_group.update();
+        // Tween_Update(dt);
+        for(let i = trees.length -1; i >= 0; --i) {
+            const tree = trees[i];
+            if(tree.is_done) {
+                Array_RemoveAt(trees, i);
+                CreateTree();
+
+                continue;
+            }
+            tree.Draw(dt);
+        }
+    end_draw();
+}
